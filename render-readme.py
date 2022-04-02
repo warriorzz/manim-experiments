c0f394d1b8b33102e3cb70ccd00c86b19a9cd19e
Bjarne Eberhardt <bjar@gmx.de>
2022-04-02 14:16:11 +0200

    Maybe I should have a look at my code

--- render-readme.py
+++ render-readme.py
@@ -6,10 +6,10 @@ scenes = glob.glob("./scenes/*.py")
 for i in range(len(scenes)):
     os.system(f"manim -ql --fps 60 --format gif --renderer opengl --write_to_movie {scenes[i]}")
 
+os.system("rm -rf ./videos")
 os.system("mkdir videos; cat ./README-TEMPLATE.md > README.md")
 videos = glob.glob("./media/videos/Scene/480p60/*.gif")
 
-os.system("rm -rf ./videos")
 for video in videos:
     os.system(f"mv {video} ./videos/{video[28::]}")
 
