f16a15828974c4a7cdd9f2d0c46cbd06f52d31bf
Bjarne Eberhardt <bjar@gmx.de>
2022-04-02 14:13:12 +0200

    Use fast render and fix name

--- render-readme.py
+++ render-readme.py
@@ -4,16 +4,17 @@ import glob
 
 scenes = glob.glob("./scenes/*.py")
 for i in range(len(scenes)):
-    os.system(f"manim -ql --fps 60 --format gif {scenes[i]}")
+    os.system(f"manim -ql --fps 60 --format gif --renderer opengl --write_to_movie {scenes[i]}")
 
 os.system("mkdir videos; cat ./README-TEMPLATE.md > README.md")
 videos = glob.glob("./media/videos/Scene/480p60/*.gif")
 
+os.system("rm -rf ./videos")
 for video in videos:
-    os.system(f"mv {video} ./videos/{video[29::]}")
+    os.system(f"mv {video} ./videos/{video[28::]}")
 
 videos = glob.glob("./videos/*.gif")
 for i in range(len(videos)):
-    name = videos[i][9:-4]
+    name = videos[i][9:-20]
     link = f"![{name}](https://github.com/warriorzz/manim-experiments/blob/main/{videos[i][2::]})"
-    os.system(f'echo "\n# {name} \n \n{link}" >> README.md')
+    os.system(f'echo "\n## {name} \n \n{link}" >> README.md')
